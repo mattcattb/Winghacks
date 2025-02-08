760eb129c84ce172636dd286644a2e6fb80f52a0
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-02-08 18:55:00 -0500

    Homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import EmbreCanvas from "./Embre.jsx";
+import Bubble from "../assets/speech-bubble-message.svg";
+
+
 function Home()
 {
     return <div className="page">
         <h1>Pl<span style={{color:"#F4D7E3"}}>a</span>n Your C<span style={{color:"#F4D7E3"}}>a</span>re</h1>
         <div className="landing-container">
-            <p> For 50 year, access to abortion</p>
+            <p> For 50 years, access to abortion was a human right protected by the states. With the recent overturn of Roe V Wade </p>
         </div>
         <div className="home-page-embre">
             <EmbreCanvas/>
+            <img src={Bubble} alt="Speech Bubble"  style={{width:"15vh"}}/>
         </div>
     </div>;
 }
