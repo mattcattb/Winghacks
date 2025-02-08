792f588d245c4f78c9843f5e9e5d855ca34eacb1
Jess Barnes <jessicabarnes@ufl.edu>
2025-02-08 15:14:04 -0500

    Silly goofy and a map

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,10 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import MapComponent from './MapComponent.jsx';
+import EmbrePillCanvas from "./EmbrePill.jsx";
+import { useState } from "react";
+import '../style.css'
 
 function Map() {
-
+    const [hoveredState, setHoveredState] = useState(null);
+        const locations = [
+            {name:'Florida', coords: [1, 2]},
+            {name: 'NY', coords: [2, 3]}
+        ]
     return <div>
-        Meow
+        <div className={"map-header"}>
+            <h1>What is the state of your state?</h1>
+        </div>
+        <div className={'map-art'}>
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -16,9 +27,11 @@ function Map() {
             </Popup>
         </Marker>
         </MapContainer>
-
-    </div>;
-
-
+        <MapComponent location={locations} />
+        </div>
+        <div className={"embre-pill-map"}>
+            <EmbrePillCanvas/>
+        </div>
+    </div>
 }
 export default Map;
