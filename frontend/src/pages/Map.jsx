2c6457ff18ce0f8de57579b1ce0d96ad08f9a2d9
Jess Barnes <jessicabarnes@ufl.edu>
2025-02-08 14:22:24 -0500

    yeah

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,25 +1,23 @@
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-import EmbrePillCanvas from './EmbrePill.jsx'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import EmbrePillCanvas from "./EmbrePill.jsx";
 
 function Map() {
 
     return <div>
-      {/*  Meow*/}
-      {/*  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>*/}
-      {/*  <TileLayer*/}
-      {/*  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
-      {/*  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'*/}
-      {/*/>*/}
-      {/*  <Marker position={[51.505, -0.09]}>*/}
-      {/*      <Popup>*/}
-      {/*      A pretty CSS3 popup. <br /> Easily customizable.*/}
-      {/*      </Popup>*/}
-      {/*  </Marker>*/}
-      {/*  </MapContainer>*/}
-        <p>ello govna</p>
+        Meow
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+        </MapContainer>
         <EmbrePillCanvas/>
-
     </div>;
 
 
