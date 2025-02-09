280df12cf63261c7e5c85f22bdc112464cf2bfca
noranguy <148975883+noranguy@users.noreply.github.com>
2025-02-09 03:43:20 -0500

    updated navbar nora from kalina branch

--- frontend/src/components/nav-bar.jsx
+++ frontend/src/components/nav-bar.jsx
@@ -1,31 +1,12 @@
 import { Link } from "react-router-dom";
-import "../style.css";
+import '../style.css';
 const Navbar = () => {
-  return (
-    <nav className="bg-purple-800 py-6 px-8 flex items-center justify-between">
-      <Link
-        className="text-white text-4xl font-bold ml-[200px] mr-[300px]"
-        to="/"
-        style={{ color: "white", textDecoration: "none" }}>
-        Home
-      </Link>
-      <div className="flex space-x-6 mr-[200px] text-2xl">
-        <Link
-          to="/map"
-          className={`text-white hover:text-gray-300 transition duration-300 ${
-            location.pathname === "/map" ? "font-bold" : "" // Highlight if active
-          }`}>
-          Map
-        </Link>
-        <Link to="/clinics" 
-              className={`text-white hover:text-gray-300 transition duration-300 ${
-                location.pathname === "/clinics" ? "font-bold" : "" // Highlight if active
-              }`}
-        >
-          ClinicFinder
-        </Link>
-      </div>
-    </nav>
-  );
-};
+    return (
+        <nav className={"nav"}>
+            <Link to="/" style={{color: "white", textDecoration: "none", fontSize: 24}}>Home</Link>
+            <Link to="/map" style={{color: "white", textDecoration: "none",  fontSize: 24}}>Map</Link>
+            <Link to="/clinics" style={{color: "white", textDecoration: "none",  fontSize: 24}}>ClinicFinder</Link>
+        </nav>
+    )
+}
 export default Navbar;
