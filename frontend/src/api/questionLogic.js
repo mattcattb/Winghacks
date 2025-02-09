ff8616b1dd62adf39e8ab1f8d6af6cd6616445cf
noranguy <148975883+noranguy@users.noreply.github.com>
2025-02-09 03:56:03 -0500

    updated question logic to main

--- frontend/src/api/questionLogic.js
+++ frontend/src/api/questionLogic.js
@@ -5,34 +5,13 @@ import {statesArray} from "../../../staticDB/states.js"
 //then, i will return information, including whether or not an abortion in possible in the state. 
 
 function questionLogic(state, weeks){
-
-    let data = null;
-    let avaAbort = false; 
-
-    for(let i = 0 ; i < statesArray.length; i++){
-        if (state === statesArray[i].abbr){
-            //we found a match, store the match somewehre? 
-            data = statesArray[i];
-            console.log(data)
-            if(weeks > data.weeksBan){
-                //condition stating that a 
-                avaAbort = false;
-            } else{
-                avaAbort = true;
-            }
-        }
-    }
-
-    let aborMess = '';
-    let weekMess = data.weeksBan;
-    
-
-    if (avaAbort === true){
-        aborMess = "You are still able to recieve an abortion at this stage! Use the ClinicFinder to find the nearest clinic"
-    } else{
-        aborMess = "Unfourtunatly, you cannot receive an abortion at this stage. Please use the ClinicFinder to locate a clinic near you outside of your state. "
-    }
-
+    console.log("QL called with:",state,weeks)
+    const data = statesArray.find(s=>s.abbr===state);
+    const avaAbort = weeks <= data.weeksBan;
+    const aborMess = avaAbort 
+    ? "You're still able to receive an abortion at this stage. Use the ClinicFinder to find the nearest clinic to you."
+    : "Unfourtunatly, you cannot receive an abortion at this stage. Please use the ClinicFinder to locate a clinic near you outside of your state.";
+    console.log("returning: ",{data,aborMess});
     return {data, aborMess};
 
 }
