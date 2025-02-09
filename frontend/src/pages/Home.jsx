8eb4c06c86dee201cc3c08cd3ad18b18c52d04b7
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-02-09 02:39:59 -0500

    formatting

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import EmbreCanvas from "../threemodels/Embre.jsx";
 function Home()
 {
-    return (
-    <div className="flex flex-col">
+    return <div className="page">
         <h1>Pl<span style={{color:"#F4D7E3"}}>a</span>n Your C<span style={{color:"#F4D7E3"}}>a</span>re</h1>
         <div className="landing-container">
             <h2> Our mission is to universalize access to clear and comprehensive information on 
@@ -19,6 +18,20 @@ function Home()
             </p>
             <p>Reproductive rights are not just a privilege, but a universal reality.</p>
         </div>
-    </div>);
+        <footer>
+        <div className="refrences">
+            <h3> Refrences: </h3>
+            <a src="https://reproductiverights.org/maps/abortion-laws-by-state/">Center for Reproductive Rights</a>
+            <a src="https://www.plannedparenthoodaction.org/abortion-access-tool/US">Planned Parenthood</a>
+            <a src = "https://www.plancpills.org/">Plan C Pills</a>
+            <a src = "https://www.usnews.com/news/best-states/articles/a-guide-to-abortion-laws-by-state">US NEWS Guide to Abortion</a>
+            <a src = "https://www.capradio.org/articles/2022/06/24/abortion-is-still-legal-in-california-here-are-answers-to-questions-about-access-in-the-state/">Capradio</a>
+"          <h3> USEFUL RESOURCES</h3>
+            <a src = "https://prochoice.org/">National Abortion Hotline: 1-800-772-9100</a>
+            <a src = "https://abortionfunds.org/">National Network of Abortion Funds</a>
+            <a src = "https://reproductivefreedomforall.org/resources/resources-for-accessing-abortion-care/"> Reproductive freedom for all</a>
+        </div>
+        </footer>
+    </div>;
 }
 export default Home;
