49455afa80ffc17e64650fa81fe91dc8bfce00fd
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-02-09 02:25:31 -0500

    Tweaked homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,6 +18,20 @@ function Home()
             </p>
             <p>Reproductive rights are not just a privilege, but a universal reality.</p>
         </div>
+        <footer>
+        <div className="refrences">
+            <h3> Refrences: </h3>
+            <a src="https://reproductiverights.org/maps/abortion-laws-by-state/">Center for Reproductive Rights</a>
+            <a src="https://www.plannedparenthoodaction.org/abortion-access-tool/US">Planned Parenthood</a>
+            <a src = "https://www.plancpills.org/">Plan C Pills</a>
+            <a src = "https://www.usnews.com/news/best-states/articles/a-guide-to-abortion-laws-by-state">US NEWS Guide to Abortion</a>
+            <a src = "https://www.capradio.org/articles/2022/06/24/abortion-is-still-legal-in-california-here-are-answers-to-questions-about-access-in-the-state/">Capradio</a>
+"          <h3> USEFUL RESOURCES</h3>
+            <a src = "https://prochoice.org/">National Abortion Hotline: 1-800-772-9100</a>
+            <a src = "https://abortionfunds.org/">National Network of Abortion Funds</a>
+            <a src = "https://reproductivefreedomforall.org/resources/resources-for-accessing-abortion-care/"> Reproductive freedom for all</a>
+        </div>
+        </footer>
     </div>;
 }
 export default Home;
