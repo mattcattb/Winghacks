8110b6ec9b3cc9afbda96d898220bace00d76543
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-02-09 03:44:38 -0500

    fixed the evil

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -14,22 +14,10 @@ function Map() {
           <h1 style={{fontSize:"2em"}}>What is the state of your state?</h1>
       </div>
       <div className={'map-art'}>
-
-    return <div>
-        Meow
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-        <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-        </MapContainer>
+        <MapComponent/>
         <EmbrePillCanvas/>
-    </div>;
+      </div>
+    </div>)
 
 
 }
