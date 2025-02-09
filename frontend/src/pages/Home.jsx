abb961c2670e11633048de4a37c656b801088dc9
noranguy <148975883+noranguy@users.noreply.github.com>
2025-02-08 20:13:13 -0500

    homepage updated, need to add references

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,10 +4,19 @@ function Home()
     return <div className="page">
         <h1>Pl<span style={{color:"#F4D7E3"}}>a</span>n Your C<span style={{color:"#F4D7E3"}}>a</span>re</h1>
         <div className="landing-container">
-            <p> Mission statement and infomrationa bout the project</p>
+            <h2> Our mission is to universalize access to clear and comprehensive information on 
+                abortion and reproductive healthcare across the United States</h2>
         </div>
         <div className="home-page-embre">
             <EmbreCanvas/>
+        </div>
+        <div className="home-page-text">
+            <p>Regardless of geography, identity, or circumstance, we believe that everyone deserves the right 
+                to make informed decisions about their own bodies. We strive to output reliable information, provide access to 
+                medical resources, and a maintain a supportive community. 
+                
+            </p>
+            <p>Reproductive rights are not just a privilege, but a universal reality.</p>
         </div>
     </div>;
 }
